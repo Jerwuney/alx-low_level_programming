536d607b35e803925f93fd4137b60d8892fe0431
Jerwuney <asorgir100@gmail.com>
2023-03-16 18:17:03 -0700

    c program 1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints a multilingual puzzle
+ * Return: 0 (The program is executed)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
