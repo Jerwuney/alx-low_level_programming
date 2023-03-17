fb8ff1acd1dbc7a9c9a619795fab60c8ef2c5d9d
Jerwuney <asorgir100@gmail.com>
2023-03-17 23:26:39 +0000

    declare

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,14 @@
  */
 int main(void)
 {
+	/* declarations */
 	char a;
 	int b;
 	long int c;
 	long long int d;
 	float f;
 
+	/* statements */
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
