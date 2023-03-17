77685fe43fdc321605e86d07385394751ece6ca3
Jerwuney <asorgir100@gmail.com>
2023-03-16 18:29:41 -0700

    art in grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints a piece of art in grammer formm
+ * Return: 0 (completed executing)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
