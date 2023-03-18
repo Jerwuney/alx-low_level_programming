d19029b0c680d56040e6bd3efbf6d2d5afcae454
Jerwuney <asorgir100@gmail.com>
2023-03-17 20:00:49 -0700

    lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints lower and uppercases alphabets
+ * Return: 0 (executed)
+ */
+
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (j = 'A', j <= 'Z'; j++)
+		putchar(j);
+	putchar('\n')
+}
