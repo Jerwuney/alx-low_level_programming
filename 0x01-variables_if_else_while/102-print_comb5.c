d2f795903412f1e765003a401507cc8145791b0a
Jerwuney <asorgir100@gmail.com>
2023-03-19 19:57:14 -0700

    final combi

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,11 +15,11 @@ int main(void)
 	for (i = 0; i <= 99; i++)
 		for (j = i + 1; j <= 99; j++)
 		{
-			putchar(i/10 + '0');
-			putchar(i%10 + '0');
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
 			putchar(' ');
-			putchar(j/10 + '0');
-			putchar(j%10 + '0');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 			if (i != 98 || j != 99)
 			{
 				putchar(',');
