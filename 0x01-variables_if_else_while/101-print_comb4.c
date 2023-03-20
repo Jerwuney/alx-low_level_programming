a464e307045102cb3141f38cd0f47fe625d0c716
Jerwuney <asorgir100@gmail.com>
2023-03-19 18:48:56 -0700

    three combi

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 		for (j = i + 1; j < 9; j++)
-			for (k = k + 1; k < 10; k++)
+			for (k = j + 1; k < 10; k++)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
