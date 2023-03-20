572d48a7b42b4fd6c9aa7e4e010b775b04ec7fe9
Jerwuney <asorgir100@gmail.com>
2023-03-19 18:42:53 -0700

    three combi

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the combinations of three digits
+ * no repetition of any combination set is allowed
+ *
+ * Return: 0 (executed)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i < 8; i++)
+		for (j = '0'; j < 9; j++)
+			for (k = '0'; k < 10; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	putchar('\n');
+	return (0);
+}
