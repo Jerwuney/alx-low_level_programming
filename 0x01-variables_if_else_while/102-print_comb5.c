7874d210065f18a3b0e90f3dc8662bccec22a96f
Jerwuney <asorgir100@gmail.com>
2023-03-19 19:51:06 -0700

    final combi

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the possible combination of
+ *two digits without repetition, 'xy' == 'yx'
+ *
+ * Return: 0 (executed)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		putchar(i/10 + '0');
+		putchar(i%10 + '0');
+		putchar(' ');
+		putchar(j/10 + '0');
+		putchar(j%10 + '0');
+		if (i != 98 || j != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
