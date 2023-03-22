862ce127850cf2295ad044d98bb22e04fcdae257
Jerwuney <asorgir100@gmail.com>
2023-03-21 19:57:11 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints and return a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	putchar('\n');
+
+	Return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C nested loops directory
