8bde2e063603b07615c42e5619e46e3b44cd6ca5
Jerwuney <asorgir100@gmail.com>
2023-03-23 22:40:59 +0000

    9 tt

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+* _islower - checks if an alphabet is lowercase
+* 
+* Return: Always 0
+*/
+
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 97 <= 122; c++)
+	{
+		_putchar(1);
+	}
+	else
+		_putchar(0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* times_table - makes the 9 times table
+*
+* Return: Always 0 (executed)
+*/
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+	int m;
+	int n;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (c > 9)
+			{
+				m = (c % 10);
+				n = (c - m) / 10;
+				_putchar(", ");
+				_putchar(n + '0');
+				_putchar(m + '0');
+			}
+			else if (b != 0)
+			{
+				_putchar(",  ");
+			}
+			_putchar(c + '0');
+		}
+		_putchar('\n');
+	}
+}
+
+}
