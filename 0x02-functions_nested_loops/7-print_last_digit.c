a250482f917ea4ad22eae4dac7a96c1c6bdcecf5
Jerwuney <asorgir100@gmail.com>
2023-03-23 10:33:12 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,6 +12,14 @@ int print_last_digit(int n)
 	int i;
 
 	i = n % 10;
-	_putchar(i + '0');
-	return (i);
+	if (i >= 0)
+	{
+		_putchar(i + '0');
+		return (i);
+	}
+	else
+	{
+		_putchar(-i + '0');
+		return (-i);
+	}
 }
