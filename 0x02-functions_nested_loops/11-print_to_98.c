5fc43cdeffdbae3e652c14aa289322af4f341cb5
Jerwuney <asorgir100@gmail.com>
2023-03-23 13:00:36 -0700

    count

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,9 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -25,13 +27,15 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
 	else
 	{
-		printf("%d, ", n);
+		printf("%d", n);
 	}
 	printf("\n");
 }
