fcd0cc7451337de1ba47fd55b7b8073c9065b420
Jerwuney <asorgir100@gmail.com>
2023-03-23 10:11:09 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - calculates absolute
+ *
+ *@n: number
+ *
+ * Return: Always 0 (executed)
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n / -1);
+	}
+}
