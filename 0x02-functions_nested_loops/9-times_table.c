64fe6556ca93384be1a976a99279cf27643e9089
Jerwuney <asorgir100@gmail.com>
2023-03-23 22:50:46 +0000

    9 tt

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,13 +23,16 @@ void times_table(void)
 			{
 				m = (c % 10);
 				n = (c - m) / 10;
-				_putchar('44 ');
+				_putchar(44);
+				_putchar(32);
 				_putchar(n + '0');
 				_putchar(m + '0');
 			}
 			else if (b != 0)
 			{
-				_putchar('44  ');
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
 			}
 			_putchar(c + '0');
 		}
