3045a2a7c3806275e3bdb81402724db4f3d73696
Jerwuney <asorgir100@gmail.com>
2023-03-23 10:22:22 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - returns 1 if lowercase, otherwise 0
+ *
+ * @n: alphabet
+ *
+ * Return: Always 0 (executed)
+ */
+
+int print_last_digit(int n)
+{
+	int i;
+
+	i = n % 10;
+	_putchar(i + '0')
+	return (i);
+}
