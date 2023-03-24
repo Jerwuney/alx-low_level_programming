7d7a9252438c3629f8d190f4cfa5cebc16017c88
Jerwuney <asorgir100@gmail.com>
2023-03-24 13:06:02 -0700

    debug

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -10,7 +10,7 @@
 void positive_or_negative(int i)
 {
 	if (i >= 0)
-		printf("%d is positive", i);
+		printf("%d is positive\n", i);
 	else
-		printf("%d is negative", i);
+		printf("%d is negative\n", i);
 }
