859bc6882525a12849f8c89522d3e0ce73b3e599
Jerwuney <asorgir100@gmail.com>
2023-03-24 12:46:19 -0700

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,12 +5,12 @@
 * Return: 0
 */
 
-void positive_or_negative(int i)
+int main(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
+	i = 98;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
