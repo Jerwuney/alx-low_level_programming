62fce2f5140ccf83776bde12e00620a864fde4d1
Jerwuney <asorgir100@gmail.com>
2023-03-24 05:16:30 -0700

    count to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,13 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 1; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j > 9)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
