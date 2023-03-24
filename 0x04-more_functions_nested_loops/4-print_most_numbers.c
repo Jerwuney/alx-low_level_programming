ca0fe1b2ee8eb3cb8d5f04869ffcea81029928b0
Jerwuney <asorgir100@gmail.com>
2023-03-24 02:41:44 -0700

    no 2 or 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,14 @@
 void print_most_numbers(void)
 {
 
-	char i;
+	int i;
 
-	for (i = '0'; i < '10'; i++)
-		if (i != '2' || i != '4')
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
 		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
-		_putchar('\n');
+	}
+	_putchar('\n');
 }
