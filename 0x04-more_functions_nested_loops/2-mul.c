697799337a4fc1be85d40f41c110997e77dbe92e
Jerwuney <asorgir100@gmail.com>
2023-03-24 01:07:19 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - check for uppercase
+ *
+ * @a: input character
+ * @b: input char
+ *
+ * Return: Always 0 (executed)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - check for uppercase
+ *
+ * Return: Always 0 (executed)
+ */
+
+void print_numbers(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+
+	return (0);
+}
