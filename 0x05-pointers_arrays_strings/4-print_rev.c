e03d9a20cab7f9b9796245d5917a7dfea895608e
Jerwuney <asorgir100@gmail.com>
2023-03-28 17:43:56 -0700

    reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,20 @@
 
 void print_rev(char *s)
 {
+	int len_s;
+
+	len_s = 0;
 	while (*s != '\0')
+	{
+		len_s++;
+		s++;
+	}
+	str--;
+	while (len_s > 0)
 	{
 		_putchar(*s);
 		s--;
+		len_s;
 	}
 	_putchar('\n');
 }
