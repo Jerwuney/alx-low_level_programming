d8522f8d650779f62acf58e2e6156a5f7da30e63
Jerwuney <--global>
2023-06-16 08:13:51 -0700

    copy memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function tha copies a memory
+ * @dest: the destination memory
+ * @src: the source memory area
+ * @n: the number of bytes to copy
+ *
+ * Return: the destination pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int d = 0;
+	int s = 0;
+
+	while (d < n)
+	{
+		dest[d] = src[s];
+		d++;
+		s++;
+	}
+	return (dest);
+}
