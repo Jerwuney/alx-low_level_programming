059cda1bd9adb540d9ea3ea2c766c25881dcdeb5
Jerwuney <--global>
2023-06-22 04:01:38 -0700

    100-set_string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -4,8 +4,6 @@
  * set_string - a function that sets the value of a pointer to a char
  * @s: first pointer
  * @to: second pointer
- *
- * Return: nothing
  */
 
 void set_string(char **s, char *to)
