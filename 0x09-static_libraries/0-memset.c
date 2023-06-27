4fd9c1cfbd7a827c34f62fb25fd1d26cc39c3e77
Jerwuney <--global>
2023-06-27 03:21:17 -0700

    static libraries

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - files a memeory with a constant byte
+ * @s: pointer to the memory area to be filled
+ * @b: constant byte value
+ * @n: number of bytes to be filled
+ *
+ *Return: the value of s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int j = 0;
+
+	while (n > 0)
+	{
+		s[j] = b;
+		j++;
+		n--;
+	}
+
+	return (s);
+}
