b1ac32633d7ecdfc8ebb64cd4125ed9acfe442a8
Jerwuney <asorgir100@gmail.com>
2023-10-12 02:26:00 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,7 +5,7 @@
  * get_op_func - uses a function to operate the maths
  * @s: the operator argument
  *
- * Return: a pointer to the right function to used
+ * Return: a pointer to the right function to use
  */
 
 int (*get_op_func(char *s))(int, int)
@@ -18,6 +18,7 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{NULL, NULL},
 	};
+
 	int j = 0;
 
 	while (ops[j].op != NULL && *(ops[j].op) != *s)
