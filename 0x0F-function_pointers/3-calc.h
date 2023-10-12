ef01ac66ca7750c8cf15f703fc06b713f3eef7f0
Jerwuney <asorgir100@gmail.com>
2023-10-12 02:10:40 -0700

    update

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -10,14 +10,14 @@
 typedef struct op
 {
 	char *op;
-	int (*f)(int x, int y);
+	int (*f)(int a, int b);
 } op_t;
 
-int op_add(int x, int y);
-int op_subt(int x, int y);
-int op_mul(int x, int y);
-int op_mod(int x, int y);
-int op_div(int x, int y);
+int op_add(int a, int b);
+int op_subt(int a, int b);
+int op_mul(int a, int b);
+int op_mod(int a, int b);
+int op_div(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,72 +1,72 @@
 #include "3-calc.h"
 
-int op_add(int x, int y);
-int op_subt(int x, int y);
-int op_mul(int x, int y);
-int op_mod(int x, int y);
-int op_div(int x, int y);
+int op_add(int a, int b);
+int op_subt(int a, int b);
+int op_mul(int a, int b);
+int op_mod(int a, int b);
+int op_div(int a, int b);
 
 /**
  * op_add - sum two numbers
- * @x: 1st number
- * @y: 2nd number
+ * @a: 1st number
+ * @b: 2nd number
  *
  * Return: the sum
  */
 
-int op_add(int x, int y)
+int op_add(int a, int b)
 {
-	return (x + y);
+	return (a + b);
 }
 
 /**
  * op_subt - calculates the difference of two numbers
- * @x: 1st input
- * @y: 2nd input
+ * @a: 1st input
+ * @b: 2nd input
  *
  * Return: the difference
  */
 
-int op_subt(int x, int y)
+int op_subt(int a, int b)
 {
-	return (x - y);
+	return (a - b);
 }
 
 /**
  * op_mul - calculates the product of two numbers
- * @x: 1st number
- * @y: 2nd number
+ * @a: 1st number
+ * @b: 2nd number
  *
  * Return: the product
  */
 
-int op_mul(int x, int y)
+int op_mul(int a, int b)
 {
-	return (x * y);
+	return (a * b);
 }
 
 /**
  * op_mod - returns the remainder after division
- * @x: 1st number
- * @y: 2nd number
+ * @a: 1st number
+ * @b: 2nd number
  *
  * Return: the remainder
  */
 
-int op_mod(int x, int y)
+int op_mod(int a, int b)
 {
-	return (x % y);
+	return (a % b);
 }
 
 /**
  * op_div - calculates the division of two numbers
- * @x: dividend
- * @y: divisor
+ * @a: dividend
+ * @b: divisor
  *
  * Return: the quotient
  */
 
-int op_div(int x, int y)
+int op_div(int a, int b)
 {
-	return (x / y);
+	return (a / b);
 }
